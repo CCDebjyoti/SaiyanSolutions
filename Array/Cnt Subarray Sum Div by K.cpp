a29a3cc98d1c4a5f57250bda794b8221717f9238
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-10-15 13:25:03 +0530

    Create Cnt Subarray Sum Div by K.cpp

---
+++ Array/Cnt Subarray Sum Div by K.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+          unordered_map<int,int>Map;
+        Map[0]=1;
+        int presum=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            presum+=nums[i];
+            int rem=presum%k;
+            if(rem<0)rem=rem+k;
+            if(Map.find(rem)!=Map.end()){
+                cnt+=Map[rem];
+            }
+            Map[rem]++;
+        }
+        return cnt;
+    }
+};
