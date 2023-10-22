7f52f621b01d9a0cc3627cb3b3c0ba9666ee3e37
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-10-22 19:40:02 +0530

    Create Merge Intervals.cpp

---
+++ Array/Merge Intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+         if(intervals.size()<=1) return intervals;
+         sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        int j=0;
+
+    for(int i=1;i<intervals.size();i++){
+        if(ans[j][1]>=intervals[i][0]){ // if in range or merge possible [1,4][4,5] --> [1,5] 
+            int t1=ans[j][0];
+            int t2=max(intervals[i][1],ans[j][1]);//if one intervals lies within another [1,4] [2,3] --> [1,4] 
+                ans.pop_back();
+                ans.push_back({t1,t2});
+            }
+            else {
+                ans.push_back(intervals[i]);
+                j++;
+            }
+
+        }
+        return ans;
+    }
+};
