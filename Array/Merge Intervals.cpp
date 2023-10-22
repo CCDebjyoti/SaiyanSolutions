0d14864fa2b9a88eec4e76a600e6a66609fefda5
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-10-22 20:04:00 +0530

    Update Merge Intervals.cpp

--- Array/Merge Intervals.cpp
+++ Array/Merge Intervals.cpp
@@ -8,6 +8,7 @@ public:
         int j=0;
 
     for(int i=1;i<intervals.size();i++){
+        // ans[j][1]=max(ans[j][1],intervals[i][1]); not stack (just chaning the last if bigger )
         if(ans[j][1]>=intervals[i][0]){ // if in range or merge possible [1,4][4,5] --> [1,5] 
             int t1=ans[j][0];
             int t2=max(intervals[i][1],ans[j][1]);//if one intervals lies within another [1,4] [2,3] --> [1,4] 
