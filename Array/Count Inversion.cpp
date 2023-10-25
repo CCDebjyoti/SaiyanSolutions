ff2e3d5cf1455cb6c0f8f807b0967b35aba10c86
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-10-25 11:49:06 +0530

    Update Count Inversion.cpp

--- Array/Count Inversion.cpp
+++ Array/Count Inversion.cpp
@@ -1,32 +1,24 @@
 int merge(vector <int> &arr, int low, int mid, int high){
-
-    vector<int> temp;
-
-    int left= low;
+vector<int> temp;
+int left= low;
     int right= mid+1;
     int cnt=0;
     while(left<=mid && right<=high){
-
-        if(arr[left]<=arr[right]){
+       if(arr[left]<=arr[right]){
         temp.push_back(arr[left++]);
-        
         }
-
         else{
         temp.push_back(arr[right++]);
         cnt+=mid-left +1;
         }
-
-    }
-
+      }
     while(left<=mid)temp.push_back(arr[left++]);
     while(right<=high)temp.push_back(arr[right++]);
-
-        for(int i=low; i<= high; i++){
+     for(int i=low; i<= high; i++){
           arr[i]= temp[i-low];
         }
+    
     return cnt;
-
 }
 
 int Solve(vector<int> & arr,int s,int e){
