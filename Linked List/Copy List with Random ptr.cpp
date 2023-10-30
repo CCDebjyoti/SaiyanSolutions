4b95d594421c44b79aad60837088a73d8f4645bc
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-10-30 18:19:24 +0530

    Update Copy List with Random ptr.cpp

--- Linked List/Copy List with Random ptr.cpp
+++ Linked List/Copy List with Random ptr.cpp
@@ -1,3 +1,4 @@
+// O(N) space 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -19,5 +20,48 @@ public:
          if(it.first -> random)it.second->random=Map[it.first->random];
         }
         return Map[head];//return clone of head 
+    }
+};
+// O(1) Space Sol
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+
+        Node*original=head;                                 
+        Node*temp=nullptr;
+     
+        while(original!=NULL){                           // 1->2->3->4->5 
+            temp=new Node(original->val);                //1->1c->2->2c->3->3c->4->4c->5->5c 
+            temp->next=original->next;
+            original->next=temp;
+            original=temp->next;
+
+        }
+        original=head;
+        Node* nhead = head->next;
+        while(original!=NULL){                  //update random 
+         if(original->random!=NULL){
+             original->next->random=original->random->next;
+            }
+            else{
+                original->next->random=nullptr;
+            }
+            original=original->next->next;
+        }
+        original=head;
+        while(original!=NULL){
+            temp = original->next; 
+            original->next = temp->next; 
+            original = original->next;
+
+            if(original){
+                temp->next = original->next;
+            } else {
+                temp->next = nullptr;
+            }
+        }
+        return nhead;
     }
 };
