959116f6fb959eb5b541003eabadb4ea300e39ff
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-11-04 21:08:52 +0530

    Lower & Upper bound.cpp

--- Binary Search/Lower & Upper bound.cpp
+++ Binary Search/Lower & Upper bound.cpp
@@ -1,8 +1,41 @@
-int lowerBound(vector<int> arr, int n, int x) {
-	//LOWER BOUND
+   //LOWER BOUND STL
 	vector<int>::iterator low = lower_bound(arr.begin(),arr.end(),x);
 	return low-arr.begin();
-	//UPPER BOUND
+    //Implementation
+    int lowerBound(vector<int> nums, int n, int target) {
+    int low=0,high = n-1;
+    int ans=n;
+       while(low<=high){
+           int mid =low +(high -low)/2;
+           
+           if(nums[mid]>=target){
+                ans=mid;
+               high=mid-1;
+           }
+           else{
+               low =mid+1;
+           }
+       }
+       return ans;
+}
+	//UPPER BOUND STL
 	vector<int>::iterator upeer=upper_bound(arr.begin(),arr.end(),x);
 	return upeer-arr.begin();
+        //Implementation
+         int lowerBound(vector<int> nums, int n, int target) {
+    int low=0,high = n-1;
+    int ans=n;
+       while(low<=high){
+           int mid =low +(high -low)/2;
+           
+           if(nums[mid]>target){
+                ans=mid;
+               high=mid-1;
+           }
+           else{
+               low =mid+1;
+           }
+       }
+       return ans;
+}
 }
