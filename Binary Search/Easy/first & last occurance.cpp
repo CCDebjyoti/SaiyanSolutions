8272441f84ca987275ec8eefac463dbd5068e66d
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-11-07 10:57:10 +0530

    Create first & last occurance.cpp

---
+++ Binary Search/Easy/first & last occurance.cpp
@@ -0,0 +1,33 @@
+vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high = n-1;
+        int first=-1,last=-1;
+        //continue search on left side 
+       while(low<=high){
+           int mid =low +(high -low)/2;
+           if(nums[mid]==target){
+               first=mid;
+               high=mid-1;
+           }
+          else if(nums[mid]>target)
+               high =mid -1 ;
+            else
+               low =mid+1;   
+       }
+       //continue search on right side
+       low=0, high=n-1;
+
+         while(low<=high){
+           int mid =low +(high -low)/2;
+           if(nums[mid]==target){
+               low=mid+1;
+               last=mid;
+           }
+          else if(nums[mid]>target)
+               high =mid -1 ;
+           else
+               low =mid+1;    
+       }
+       return {first,last};
+       
+    }
