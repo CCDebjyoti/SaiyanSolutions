5f94f1e095f6dae43b2ff6c39b558a1a0c4b5d51
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-11-14 13:10:35 +0530

    Create sqrt of Nth root of m.cpp

---
+++ Binary Search/Medium/sqrt of Nth root of m.cpp
@@ -0,0 +1,17 @@
+int NthRoot(int n, int m) {
+  // Write your code here.
+  long long low=0,high=m;
+    while(low<=high){
+         long long mid=low+(high-low)/2;
+        if(pow(mid,n)==m){
+        return mid;
+        }
+        else if(pow(mid,n)>m){
+            high=mid-1;
+        }
+        else {
+            low=mid+1;
+        }
+    }
+    return -1;
+}
