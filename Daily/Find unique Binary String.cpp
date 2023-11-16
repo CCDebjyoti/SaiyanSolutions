0427b82405d1677aa580838854be5b6b2eb776b5
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-11-16 23:24:02 +0530

    Update Find unique Binary String.cpp

--- Daily/Find unique Binary String.cpp
+++ Daily/Find unique Binary String.cpp
@@ -1,3 +1,15 @@
+
+//M-3 Most optimal O(N) O(1) // we know we want diffrent to inp so why not change 1st place for 1st string 2nd for 2nd 
+//eg -> 111 101 111 ->ans = 0 _ _ now 01 _ then 010 we got ans
+//to make diffrent from ip we change one diffrence in curr string can be any here i changed if 1 -> 0 to get diff string 
+string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+        for(int i=0;i<nums.size();i++){
+            ans+=nums[i][i]=='0'?'1':'0';
+        }
+        return ans;
+    }
+
 class Solution {
 public:
    string ans;
@@ -26,16 +38,4 @@ public:
 //convert string to number eg -> 111 - 7 how ? stoi(nums[i],0,2) (str,starting pos,base) put in set 
 //check for 0-> n+1 in set if not found convert back ans=bitset<16>(num).to_string();
 //we only need last n digits so substr(16-n)
-
-
-//M-3 Most optimal O(N) O(1) // we know we want diffrent to inp so why not change 1st place for 1st string 2nd for 2nd 
-//eg -> 111 101 111 ->ans = 0 _ _ now 01 _ then 010 we got ans
-//to make diffrent from ip we change one diffrence in curr string can be any here i changed if 1 -> 0 to get diff string 
-string findDifferentBinaryString(vector<string>& nums) {
-        string ans;
-        for(int i=0;i<nums.size();i++){
-            ans+=nums[i][i]=='0'?'1':'0';
-        }
-        return ans;
-    }
 
