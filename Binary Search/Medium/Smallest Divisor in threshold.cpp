b02db133d8b943fc69b6df95991d6868c5764a02
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2023-11-16 19:51:05 +0530

    Create Smallest Divisor in threshold.cpp

---
+++ Binary Search/Medium/Smallest Divisor in threshold.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long solve(vector<int>& nums, int mid){
+        long long cnt=0;
+        for(int i=0;i<nums.size();i++){
+         cnt+=ceil(nums[i]/(double)mid);
+        }
+        return cnt;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n=nums.size();
+        int low=1;
+        int high=*max_element(nums.begin(),nums.end());
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long cnt=solve(nums,mid);
+            if(cnt<=threshold){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+
+        }
+        return solve(nums,1);
+    }
+};
