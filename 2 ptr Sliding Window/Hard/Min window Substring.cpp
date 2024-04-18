3c71188cb52584fa97b7842d926b68bbf273e9d0
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2024-04-18 20:51:02 +0530

    Update Min window Substring.cpp

--- 2 ptr Sliding Window/Hard/Min window Substring.cpp
+++ 2 ptr Sliding Window/Hard/Min window Substring.cpp
@@ -1,36 +1,34 @@
-class Solution {//where upadte answer 
+class Solution {
 public:
     string minWindow(string s, string t) {
         int m=t.size();
         int n=s.size();
-        /* if(m>n)return ""; */
+        if(m>n)return ""; 
         int stri=0,strj=0;
         int i=0,j=0,cnt,mini=INT_MAX;
         unordered_map<int,int>Map;
-        for(int i=0;i<m;i++)Map[t[i]]++;
+        for(int i=0;i<m;i++)Map[t[i]]++;//hash t in map
         cnt=Map.size();
         while(j<n){
-            if(Map.find(s[j])!=Map.end()){
+            if(Map.find(s[j])!=Map.end()){//sldiding window template eg->Map[s[j]]++
                 Map[s[j]]--;
                 if(Map[s[j]]==0)cnt--;
             }
             
-         if(cnt==0){
-         while(cnt==0){
-         if(Map.find(s[i])!=Map.end()){
-                Map[s[i]]++;
-                if(Map[s[i]]==1){
-                cnt++;
-                if(j-i+1<mini){
+         while(cnt==0){//optimise window size if possible shrink it 
+        if(j-i+1<mini){//update answer 
                  mini=j-i+1;
                  stri=i;
                  strj=j;
             }
+         if(Map.find(s[i])!=Map.end()){
+                Map[s[i]]++;
+                if(Map[s[i]]==1){
+                cnt++;
                 }
             }
          i++;
          }
-        }
         j++;
         }
         string r = s.substr(stri, strj-stri+1);
