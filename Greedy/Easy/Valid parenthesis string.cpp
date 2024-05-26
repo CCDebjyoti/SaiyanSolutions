3527ad3078035498d76341392ad86d63e52104ff
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2024-05-26 16:57:52 +0530

    Valid parenthesis string.cpp

--- Greedy/Easy/Valid parenthesis string.cpp
+++ Greedy/Easy/Valid parenthesis string.cpp
@@ -1,5 +1,5 @@
 
-    bool checkValidString(string s) {// we store index not values 
+    bool checkValidString(string s) {// we store index not values not star oc or cc  we store i 
         stack<int>open;//store open bracket 
         stack<int>astr;//store star here 
         for(int i=0;i<s.size();i++){
@@ -17,4 +17,17 @@
             astr.pop();
         }
         return open.empty();
+    }
+  //two ptr
+   bool checkValidString(string s) {
+        int n=s.size();
+        int oc=0,cc=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='*')oc++;//assume all astrx as ( 
+            else oc--;
+            if(s[n-1-i]==')'||s[n-1-i]=='*')cc++;//assume all astrx as )
+            else cc--;
+            if(oc<0 || cc<0)return false;//if -ve we it dosenot have any * or () to validate it 
+        }
+        return true;
     }
