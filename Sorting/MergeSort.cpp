f27c30e926d00784c2218243b28bab531cea1109
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2024-06-07 01:10:32 +0530

    Update MergeSort.cpp

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -1,68 +1,38 @@
-//single temp array
-void merge(vector <int> &arr, int low, int mid, int high){
-    vector<int> temp;
-    int left= low;
-    int right= mid+1;
-
-    while(left<=mid && right<=high){
-        if(arr[left]<=arr[right])temp.push_back(arr[left++]);
-            
-        else temp.push_back(arr[right++]);
-    }
-    while(left<=mid)temp.push_back(arr[left++]);
-    while(right<=high)temp.push_back(arr[right++]);
-    
-    for(int i=low; i<= high; i++){
-    arr[i]= temp[i-low];
-    }
-}
-//two array and join 
-void mergeSorted(vector<int> &arr, int s, int mid, int e){
-
-    int len1 = mid-s+1;
-    int len2 = e-mid;
-    
-    vector<int> arr1(len1);
-    vector<int> arr2(len2);
-
-    for(int i=0; i<len1; i++){
-        arr1[i] = arr[s+i];
-    }
-
-    for(int i=0; i<len2; i++){
-        arr2[i] = arr[mid+i+1];
-    }
-
-    int i=0;
-    int j=0;
-    int k=s;
-    while(i<len1 && j<len2 ){
-        if(arr1[i]>arr2[j]){
-            arr[k++]=arr2[j++];
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array//to merge and store 
+    int left = low;      // starting index of left half of arr// [0,2] left is 0
+    int right = mid + 1;   // right is 2 as two arrays [0,1] and [2]
+    //storing elements in the temporary array in a sorted manner//
+    while (left <= mid && right <= high) {//store smaller element first 
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
         }
         else {
-            arr[k++]=arr1[i++];
+            temp.push_back(arr[right]);
+            right++;
         }
     }
-    while(i<len1)arr[k++]=arr1[i++];
-
-    while(j<len2)arr[k++]=arr2[j++];
+    // if elements on the left half are still left //elements still left on any arrays
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    // transfering all elements from temporary to arr //[3,5] arr[3]=temp[3-3]-->temp[0] for arr[4]=temp[4-3]--temp[1]
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
 }
-
-void Solve(vector<int> & arr,int s,int e){
-    int mid=(s+e)/2;
-    //base case
-    if(s>=e)return;
-    //left sort 
-    Solve(arr,s,mid);
-    //right
-    Solve(arr,mid+1,e);
-    //Merge
-    mergeSorted(arr,s,mid,e);
-}
-
-void mergeSort(vector < int > & arr, int n) {
-
-    // Write your code here.
-    Solve(arr,0,n-1);
+//
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
 }
