75a7acf053a9ac1db850570fa0d90f85498b0bb0
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-02-16 12:59:59 +0530

    Update Non Overlapping intervals.cpp

--- Greedy/Medium/Non Overlapping intervals.cpp
+++ Greedy/Medium/Non Overlapping intervals.cpp
@@ -2,21 +2,19 @@ bool comp(vector<int> &a,vector<int> &b) {
 	return a[1]<b[1];
 }//sort using end times as priority to intervals finish early 
 class Solution {
-public:
+public://we want to remove least no of intervals which is only possible when 
+//we sort with end times as that leads to least no of intervals overlap 
     int eraseOverlapIntervals(vector<vector<int>>& ix) {
         //if(ix.size()==1)return 0;
-        vector<pair<int,int>>stor;//sort based on first index 
+        //sort based on first index 
         sort(ix.begin(),ix.end(),comp);
-        stor.push_back({ix[0][0],ix[0][1]});
         int ans=0;
         int j=0;
+        int compx=ix[0][1];
         for(int i=1;i<ix.size();i++){
-          if(stor[j].second<=ix[i][0]){
-          stor.push_back({ix[i][0],ix[i][1]});
-            j++;
-          }
+          if(compx>ix[i][0])ans++;
+          else compx=ix[i][1];
         }
-        ans=ix.size()-stor.size();
         return ans;
     }
 };
