b72bd4ff2cb20b3c6e756c2ac815c48cdb99bf33
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-03-06 21:09:23 +0530

    Update Max depth.cpp

--- Binary Tree/Easy/Max depth.cpp
+++ Binary Tree/Easy/Max depth.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL)
+        if(root == NULL)//TC-O(N) SC-O(N)
             return 0;
         int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
