e6cd509ae36ac41d6f9d94c1bdf8398871413aac
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-03-13 23:16:24 +0530

    Update Right View of Binary Tree.cpp

--- Binary Tree/Medium/Right View of Binary Tree.cpp
+++ Binary Tree/Medium/Right View of Binary Tree.cpp
@@ -1,24 +1,19 @@
 vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-        if(root==NULL)return ans;
-        queue<pair<TreeNode*,int>>q;//level and node
-        map<int,int>Map;//level,value
-        q.push({root,0});
+        if(root==NULL)return ans;//edge case 
+        queue<TreeNode*>q;
+        q.push(root);
+        TreeNode* temp;
         while(!q.empty()){
-            auto it=q.front().first;
-            TreeNode*temp=q.front().first;
-            int level=q.front().second;
-            q.pop();
-            if(Map.find(level)==Map.end()){
-                Map[level]=temp->val;
+            int sizeq=q.size();
+            vector<int>level;
+            for(int i=0;i<sizeq;i++){//just level order traversal add first node and add right first 
+                temp=q.front();
+                q.pop();
+                if(temp->right)q.push(temp->right);
+                if(temp->left)q.push(temp->left);
+                if(i==0)ans.push_back(temp->val);
             }
-            if(temp->right){
-                q.push({temp->right,level+1});
-            }
-            if(temp->left)q.push({temp->left,level+1});
-        }
-        for(auto it:Map){
-            ans.push_back(it.second);
         }
         return ans;
     }
