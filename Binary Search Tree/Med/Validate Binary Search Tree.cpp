53716bc155ea21b7584a6243a475a7a42a9c725b
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-04-04 22:17:47 +0530

    Update Validate Binary Search Tree.cpp

--- Binary Search Tree/Med/Validate Binary Search Tree.cpp
+++ Binary Search Tree/Med/Validate Binary Search Tree.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool solve(TreeNode*root,long long mini,long long maxi){
         if(root==NULL)return true;
-        if(root->val>=maxi || root->val<=mini)return false;//Must lie in the range of maxi to mini
-        return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);//for left the root->val is the max element allowed 
-    }//for right the min val of the subtree must be greater than mini root->val
+        if(root->val>=maxi || root->val<=mini)return false;
+        return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
+    }/
     bool isValidBST(TreeNode* root) {
        return solve(root,LLONG_MIN,LLONG_MAX);
     }
