fd06244cb4a110de5658229177d585c36a17c149
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-04-04 22:18:07 +0530

    Update Validate BST.cpp

--- Binary Search Tree/Med/Validate BST.cpp
+++ Binary Search Tree/Med/Validate BST.cpp
@@ -2,8 +2,8 @@ class Solution {//every node must be in some range
 public://for left it cannot have a val greater than root
     bool solve(TreeNode*root,long long mini,long long maxi){//for right it cant have any val lessser than min
         if(root==NULL)return true;
-        if(root->val>=maxi || root->val<=mini)return false;
-        return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
+        if(root->val>=maxi || root->val<=mini)return false;//for left the root->val is the max element allowed 
+        return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);//for right the min val of the subtree must be greater than mini root->val
     }
     bool isValidBST(TreeNode* root) {
        return solve(root,LLONG_MIN,LLONG_MAX);
