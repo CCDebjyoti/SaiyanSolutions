3d58e5dc6c3c9d3af30d5879f162dda100d61129
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-04-04 22:33:43 +0530

    Update Validate Binary Search Tree.cpp

--- Binary Search Tree/Med/Validate Binary Search Tree.cpp
+++ Binary Search Tree/Med/Validate Binary Search Tree.cpp
@@ -4,7 +4,7 @@ public:
         if(root==NULL)return true;
         if(root->val>=maxi || root->val<=mini)return false;
         return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
-    }/
+    }
     bool isValidBST(TreeNode* root) {
        return solve(root,LLONG_MIN,LLONG_MAX);
     }
