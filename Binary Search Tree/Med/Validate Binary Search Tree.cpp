1b796ec2cae3ee2abd3e9d706f702bc464bcfe0b
DebjyotiDas <139075911+CCDebjyoti@users.noreply.github.com>
2025-04-05 20:41:29 +0530

    Update Validate Binary Search Tree.cpp

--- Binary Search Tree/Med/Validate Binary Search Tree.cpp
+++ Binary Search Tree/Med/Validate Binary Search Tree.cpp
@@ -1,11 +1,37 @@
 class Solution {
 public:
-    bool solve(TreeNode*root,long long mini,long long maxi){
-        if(root==NULL)return true;
-        if(root->val>=maxi || root->val<=mini)return false;
-        return solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
+    TreeNode*findlastright(TreeNode*root){//last right points to left child
+        if(root->right==NULL)return root;//we connect root to key->left 
+        return findlastright(root->right);
     }
-    bool isValidBST(TreeNode* root) {
-       return solve(root,LLONG_MIN,LLONG_MAX);
+    TreeNode*helper(TreeNode*root){
+        if(root->right==NULL)return root->left;//just point root to left
+        else if(root->left==NULL)return root->right;
+        TreeNode*rightchild=root->right;
+        TreeNode*Lastright=findlastright(root->left);//last right of left subtree smaller than entire right sibtree
+        Lastright->right=rightchild;
+        return root->left;//as we have connected lastright of left subtree to right child 
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {//O(height of tree)
+        if(root==NULL)return root;
+        if(root->val==key)return helper(root);
+        TreeNode*curr=root;
+        while(curr!=NULL){
+            if(curr->val>key){
+             if(curr->left!=NULL && curr->left->val==key){
+              curr->left=helper(curr->left);
+              break;
+             }
+             else curr=curr->left;
+            }
+            else {
+                if(curr->right!=NULL && curr->right->val==key){
+                    curr->right=helper(curr->right);
+                    break;
+                }
+                else curr=curr->right;
+            }
+        }
+        return root;
     }
 };
